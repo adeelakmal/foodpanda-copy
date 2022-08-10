8ceee89b6c6f1d41aba1a097cfe6720bdedac6ce
adeelakmal <adeelakmal0@gmail.com>
2022-08-10 15:31:36 +0500

    dont remove this dumb bitch arhum
    i will kill you and eat you... YUMMMMMMM

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -10,53 +10,72 @@ function Cities() {
   ];
   return (
     <>
-      <div className="relative  citiesMargin">
-        <h2 className="absolute top-0 font-Open font-bold text-gray-300/25 px-12 fontCities">
+      <div className="absolute citiesMargin">
+        <h2 className="relative top-0 font-Open font-bold text-gray-300/25 px-12 fontCities">
           Cities
         </h2>
-        <span className="text-lg font-light leading-big ManyMore"> {/*many more has relative*/}
+        <span className="absolute top-0 text-lg font-light leading-big pt-20 pl-20 ManyMore">
           Find us in these cities and many more!
         </span>
+
         {/* get the city names and pictures that have already been stored in a database use mongo probably */}
-        <ul className="relative block top-2">
-          <li>
-            <div className="w-64">
-              <div className="grid grid-rows-1 grid-flow-col gap-7 citiesMargin">
-              <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
-                <img className="object-cover h-64 w-48 min-h-[300px] min-w-[230px] citiesHover" src={cities[0]} alt="Laor"/>
-                <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
-                  Lahore
-                </figcaption>
-              </figure>
-              <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
-                <img className="object-cover h-64 w-48 min-h-[300px] min-w-[230px] citiesHover" src={cities[1]} alt="Laor" />
-                <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
-                  karachi
-                </figcaption>
-              </figure>
-              <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
-                <img className="object-cover h-64 w-48 min-h-[300px] min-w-[230px] citiesHover" src={cities[2]} alt="Laor" />
-                <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
-                  Islamabad
-                </figcaption>
-              </figure>
-              <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
-                <img className="object-cover h-64 w-48 min-h-[300px] min-w-[230px] citiesHover" src={cities[3]} alt="Laor" />
-                <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
-                  Faisalabad
-                </figcaption>
-              </figure>
-              <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
-                <img className="object-cover h-64 w-48 min-h-[300px] min-w-[230px] citiesHover" src={cities[4]} alt="Laor" />
-                <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
-                  Rawalpindi
-                </figcaption>
-              </figure>
-              </div>
-            </div>
-          </li>
-        </ul>
       </div>
+      <ul className="mt-44 mx-20">
+        <li>
+          <div className="grid grid-rows-1 grid-flow-col">
+            <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
+              <img
+                className="object-cover h-64 w-48 min-h-[150px] min-w-[100px] citiesHover"
+                src={cities[0]}
+                alt="Laor"
+              />
+              <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
+                Lahore
+              </figcaption>
+            </figure>
+            <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
+              <img
+                className="object-cover h-64 w-48 min-h-[150px] min-w-[100px]  citiesHover"
+                src={cities[1]}
+                alt="Laor"
+              />
+              <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
+                karachi
+              </figcaption>
+            </figure>
+            <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
+              <img
+                className="object-cover h-64 w-48 min-h-[150px] min-w-[100px]  citiesHover"
+                src={cities[2]}
+                alt="Laor"
+              />
+              <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
+                Islamabad
+              </figcaption>
+            </figure>
+            <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
+              <img
+                className="object-cover h-64 w-48 min-h-[150px] min-w-[100px]  citiesHover"
+                src={cities[3]}
+                alt="Laor"
+              />
+              <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
+                Faisalabad
+              </figcaption>
+            </figure>
+            <figure className="relative block h-full bg-no-repeat bg-center bg-cover">
+              <img
+                className="object-cover h-64 w-48 min-h-[150px] min-w-[100px]  citiesHover"
+                src={cities[4]}
+                alt="Laor"
+              />
+              <figcaption className="relative bottom-1/4 text-4xl font-slim text-white px-1">
+                Rawalpindi
+              </figcaption>
+            </figure>
+          </div>
+        </li>
+      </ul>
     </>
   );
 }
