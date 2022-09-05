f3979d9ece880c7dd011b5bf34731727a70b91d8
adeelakmal <adeelakmal0@gmail.com>
2022-09-05 23:44:45 +0500

    cities changes
    the city is changing

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,27 +1,26 @@
 import React from "react";
-import useWindowDimensions from './Dimentions';
-import Test from "./Test";
+import useWindowDimensions from "./Dimentions";
+// import Test from "./Test";
 function Section() {
   const { width } = useWindowDimensions();
-  if (width>800)
-  {
-  return (
-    <>
-      <div className="mb-6">
-        <div className="img"></div>
+  if (width > 800) {
+    return (
+      <>
+        <div className="mb-6">
+          <div className="img"></div>
           <div className="hero-sec relative pt-20">
             <div className="hero-text pt-20 pb-12">
               <h1 className="text-4xl font-Open font-light">
-                It's the food and groceries you love, delivered {width}
+                It's the food and groceries you love, delivered
               </h1>
-          </div>
+            </div>
 
-          <div className="md:w-3/4 w-[100%] flex gap-4 p-3 bg-white rounded-lg drop-shadow-lg">
-            <input
-              className="flex-auto w-[60%] col-span-3 p-4 border border-gray border-solid rounded-lg"
-              type="text"
-              placeholder="Search for anything..." 
-            />
+            <div className="md:w-3/4 w-[100%] flex gap-4 p-3 bg-white rounded-lg drop-shadow-lg">
+              <input
+                className="flex-auto w-[60%] col-span-3 p-4 border border-gray border-solid rounded-lg"
+                type="text"
+                placeholder="Search for anything..."
+              />
               <button className="flex-intial bg-pink text-white rounded-lg w-44">
                 Delivery
               </button>
@@ -29,30 +28,28 @@ function Section() {
               <button className="flex-intial bg-pink text-white rounded-lg w-44">
                 Pick-Up
               </button>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-}
-else
-{
-  return (<>
-  <div className="mb-6">
-    <div className="img"></div>
-      <div className="hero-sec relative pt-20">
-        <div className="hero-text pt-20 pb-12">
-          <h1 className="text-4xl font-Open font-light">
-            It's the food and groceries you love, delivered
-          </h1>
-      </div>
-      
-      </div>
-    </div>
-    <Test/>
-</>
-);
-}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="mb-6">
+          <div className="img"></div>
+          <div className="hero-sec relative pt-20">
+            <div className="hero-text pt-20 pb-12">
+              <h1 className="text-4xl font-Open font-light">
+                It's the food and groceries you love, delivered
+              </h1>
+            </div>
+          </div>
+        </div>
+        {/* <Test /> */}
+      </>
+    );
+  }
 }
 
 export default Section;
